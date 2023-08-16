150815f24209316bfd823434115fc6ed028ef827
Rian Furtado <90117071+Rian-Furtado@users.noreply.github.com>
2023-08-16 00:43:21 +0000

    adiciona página

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,23 @@
 function Home() {
-  return <h1>Página inicial 😎</h1>;
+  const underConstructionStyles = {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "column",
+    fontSize: "34px",
+    color: "BLACK",
+    textAlign: "center",
+    paddingTop: "50px",
+    height: "100vh",
+    backgroundColor: "WHITE",
+  };
+
+  return (
+    <body style={underConstructionStyles}>
+      <span>🚧</span>
+      <strong>EM CONSTRUÇÃO, UM LOCAL COM CONTEÚDOS DE VALOR CONCRETO</strong>
+    </body>
+  );
 }
 
 export default Home;
